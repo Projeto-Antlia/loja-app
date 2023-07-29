d1f37e14e3a1936e4c03126763c9f8c872d4f2b3
Anderson Daniel <anderbreiksondaniel@gmail.com>
2023-07-29 14:20:38 -0300

    #6  modalConfirmation

--- front/antlia-app/src/components/TotalMkt/TotalMkt.tsx
+++ front/antlia-app/src/components/TotalMkt/TotalMkt.tsx
@@ -1,11 +1,28 @@
 import { HStack, KeyboardAvoidingView, NativeBaseProvider, Text, Image, Pressable, Box, Button, FormControl, Input, VStack } from "native-base";
-import { Alert, Modal } from "react-native";
-import React, { useState } from "react";
+import { Alert, Modal, TouchableOpacity, View } from "react-native";
+import React, { useEffect, useState } from "react";
 import theme from "../../theme";
-
 
 export default function TotalMkt() {
     const [modalVisible, setModalVisible] = useState(false);
+    const [count, setCount] = useState(15);
+
+    useEffect(() => {
+        let interval: any;
+        if(modalVisible){
+            interval = setInterval(() => {
+                setCount((prev) => prev - 1);
+            }, 1000);
+        }
+        return() => clearInterval(interval);
+    }, [modalVisible]);
+
+    useEffect(() => {
+        if( count === 0 ){
+            setModalVisible(false);
+            setCount(15);
+        }
+    }, [count]);
     return (
         <NativeBaseProvider>
             <KeyboardAvoidingView>
@@ -16,18 +33,44 @@ export default function TotalMkt() {
                     onRequestClose={() => {
                         Alert.alert('Modal has been closed.');
                         setModalVisible(!modalVisible);
+                        setCount(15);
                     }}>
-                    <Box w='70%' h='50%' bg={theme.colors.white} marginX={'15%'} marginY={'20%'} rounded={30} alignItems={"center"} justifyContent={'space-around'}>
-                        <VStack alignItems={"center"} w='80%'>
-                            <Image source={require('../../assets/handsCard.png')} />
-                            <Text  fontSize={20} fontFamily={theme.fonts.bold} textAlign={'center'} >
-                                APROXIME O SEU CARTÃO NO LEITOR PARA CONFIRMAR {'\n'} A SUA COMPRA
-                            </Text>
-                            <Pressable onPress={() => setModalVisible(!modalVisible)}>
-                                <Text>X</Text>
-                            </Pressable>
-                        </VStack>
-                    </Box>
+                    <TouchableOpacity
+                        style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
+                        activeOpacity={1}
+                        onPress={() => setModalVisible(!modalVisible)}>
+                        <View style={{
+                            backgroundColor: "rgba(0, 0, 0, 0.5)",
+                            position: "absolute",
+                            top: 0,
+                            left: 0,
+                            right: 0,
+                            bottom: 0,
+                        }}
+                        />
+                        <Box
+                            w="60%"
+                            h="40%"
+                            bg={theme.colors.white}
+                            marginX={"15%"}
+                            marginY={"20%"}
+                            rounded={30}
+                            alignItems={"center"}
+                            justifyContent={"space-around"}>
+                            <VStack alignItems={"center"} w='80%'>
+                                <Text fontSize={20} fontFamily={theme.fonts.bold} textAlign={'center'} mb='5%' >
+                                    0:{count}
+                                </Text>
+                                <Image w='220' h='200' source={require('../../assets/handsCard.png')} />
+                                <Text fontSize={18} fontFamily={theme.fonts.bold} textAlign={'center'} mt='10%' >
+                                    APROXIME O SEU CARTÃO NO LEITOR PARA CONFIRMAR {'\n'} A SUA COMPRA
+                                </Text>
+                                {/* <Pressable onPress={() => setModalVisible(!modalVisible)}>
+                                    <Text>X</Text>
+                                </Pressable> */}
+                            </VStack>
+                        </Box>
+                    </TouchableOpacity>
                 </Modal>
                 <HStack bg={theme.colors.white} alignItems={'center'} justifyContent={'space-around'} h='100px'>
                     <Text color={theme.colors.textPrimary} fontSize={20} style={{ fontFamily: 'Rubik_400Regular' }}>
