0f011af263a300479a8e2c7931c1d5e79ea63377
Thiago Alves <thiago_marketingdigital@hotmail.com>
2023-08-25 14:14:48 -0300

    add swagger

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,9 +2,25 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
+import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
+
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Projeto Antlia')
+    .setDescription('Antlia API')
+    .setVersion('1.0')
+    .addTag('antlia')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   app.useGlobalPipes(new ValidationPipe());
+
   await app.listen(3000);
 }
+
 bootstrap();
